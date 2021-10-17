33691ebbc90a3529ad95e90b7b866b3fd9e51b50
Albert Minnie <albert.minnie@gmail.com>
2021-10-17 07:42:46 +0200

    add names for functions

--- scripts/generate_files.py
+++ scripts/generate_files.py
@@ -3,8 +3,14 @@ import csv
 with open('../bin/code.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     objectSet = set()
+    f = open('new_names.idc', 'w')
     for row in csv_reader:
         objectSet.add(row['ObjectFile'])
+        addrHex = "0x" + row['Address']
+        offset = int("0x401000", 16)
+        addrInt = int(addrHex, 16) + offset
+        f.write('    MakeName({0}, "{1}");\n'.format(hex(addrInt), row['MangledName']))
+    f.close()
     for object in objectSet:
         if '.obj' in object and not ':' in object:
             f = open("./tmp/" + object.replace('.obj', '.cpp'), "w")
