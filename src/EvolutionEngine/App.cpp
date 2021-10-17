8fbac9b65b0bfdfcbfbd2e472d1abc2370798214
Albert Minnie <albert.minnie@gmail.com>
2021-10-17 12:31:11 +0200

    add 2 (empty) methods to app

--- src/EvolutionEngine/App.cpp
+++ src/EvolutionEngine/App.cpp
@@ -10,4 +10,16 @@ namespace EvolutionEngine
         return &ourApp;
     }
 
+    // 0x0054E000
+    void App::UpdateApp()
+    {
+
+    }
+
+    // 0x00550E10
+    void App::DoLoadWorld(const std::string &name)
+    {
+
+    }
+
 }
--- src/EvolutionEngine/App.h
+++ src/EvolutionEngine/App.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <string>
+
 namespace EvolutionEngine
 {
 
     class App
     {
     public:
+        void UpdateApp();
+        void DoLoadWorld(const std::string &name);
     };
 
     App *GetTheApp();
