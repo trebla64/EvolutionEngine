af91a8ba0db6307526628c5b2aefee7b36d8f1b6
Albert Minnie <albert.minnie@gmail.com>
2021-10-17 08:00:57 +0200

    add entry point

--- src/EvolutionEngine/Window.cpp
+++ src/EvolutionEngine/Window.cpp
@@ -1,3 +1,13 @@
+#if _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
 #include "Window.h"
 
-// TODO: Implement
+#if _WIN32
+// 0x00477280
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return 0;
+}
+#endif
